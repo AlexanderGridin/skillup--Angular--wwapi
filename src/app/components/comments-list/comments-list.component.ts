6e1a2954564826591a285de133e993d705b0db93
Alexander Gridin <alexander.m.gridin@gmail.com>
2021-09-23 12:37:13 +0300

    comments-list component completed

--- src/app/components/comments-list/comments-list.component.ts
+++ src/app/components/comments-list/comments-list.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Comment } from 'src/app/interfaces/comment';
 
 @Component({
   selector: 'comments-list',
   templateUrl: './comments-list.component.html',
   styleUrls: ['./comments-list.component.css'],
 })
-export class CommentsListComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class CommentsListComponent {
+  @Input() public comments!: Comment[];
 }
