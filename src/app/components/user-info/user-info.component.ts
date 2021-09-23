46ad5e6ba5569d3813afc974f7575a35c02d3945
Alexander Gridin <alexander.m.gridin@gmail.com>
2021-09-23 18:16:13 +0300

    user-info component was implemented

--- src/app/components/user-info/user-info.component.ts
+++ src/app/components/user-info/user-info.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from 'src/app/interfaces/user/user';
+import { UserOfUserInfo } from 'src/app/interfaces/user/user-of-user-info';
+import { UserMapper } from 'src/app/mappers/UserMapper';
 
 @Component({
-  selector: 'app-user-info',
+  selector: 'user-info',
   templateUrl: './user-info.component.html',
-  styleUrls: ['./user-info.component.css']
+  styleUrls: ['./user-info.component.css'],
 })
 export class UserInfoComponent implements OnInit {
+  @Input() public user!: User;
+  public userForRender!: UserOfUserInfo;
 
-  constructor() { }
+  constructor(private userMapper: UserMapper) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.userForRender = this.userMapper.prepareForRenderInUserInfo(this.user);
   }
-
 }
--- src/app/mappers/UserMapper.ts
+++ src/app/mappers/UserMapper.ts
@@ -1,6 +1,7 @@
 import { User } from '../interfaces/user/user';
 import { UserOfUsersTable } from '../interfaces/user/user-of-users-table';
 import { AddUserFormData } from '../interfaces/form-data/add-user-form-data';
+import { UserOfUserInfo } from '../interfaces/user/user-of-user-info';
 
 export class UserMapper {
   public prepareForRenderInUsersTable(user: User): UserOfUsersTable {
@@ -10,6 +11,17 @@ export class UserMapper {
       email: user.email,
       address: `${user.address.city}, ${user.address.street}, ${user.address.building}`,
       phone: user.phone,
+    };
+  }
+
+  public prepareForRenderInUserInfo(user: User): UserOfUserInfo {
+    return {
+      fullName: `${user.firstName} ${user.lastName}`,
+      email: user.email,
+      address: `${user.address.zipcode}, ${user.address.city}, ${user.address.street}, ${user.address.building}`,
+      phone: user.phone,
+      website: user.website,
+      companyInfo: `${user.company.name}, ${user.company.scope}`,
     };
   }
 
