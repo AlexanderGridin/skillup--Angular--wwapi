bc66139e8e1a806738053a9d9d646c773798fae5
Alexander Gridin <alexander.m.gridin@gmail.com>
2021-09-24 12:33:43 +0300

    post component refactored

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -14,6 +14,7 @@ import { Comment } from 'src/app/interfaces/comment';
 })
 export class PostComponent implements OnInit, OnDestroy {
   @Input() post!: Post;
+
   public comments!: Comment[];
 
   public isEditing: boolean = false;
